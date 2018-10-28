96b1894576bbcb24c1974326f41aa779dcb85ac0
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-10-28 13:36:35 +0800

    modified index.html modified app.js

--- test/election.js
+++ test/election.js
@@ -1,6 +1,7 @@
 var Election = artifacts.require("./Election.sol")
 
 contract("Election", function(accounts){
+    var electionInstance;
 
 	it("initializes with two candidates", function(){
 		return Election.deployed().then(function(instance){
@@ -14,13 +15,13 @@ contract("Election", function(accounts){
 	    return Election.deployed().then(function(instance){
 	        electionInstance = instance;
 	        return electionInstance.candidates(1);
-	    }).then(function(candidate){
-	        assert.equal(candidate[0],1, "contains the correct id");
+	    }).then(function(candidate) {
+	        assert.equal(candidate[0], 1, "contains the correct id");
 	        assert.equal(candidate[1], "Candidate 1", "contains the correct name");
 	        assert.equal(candidate[2], 0, "contains the correct vote count");
 	        return electionInstance.candidates(2);
-	    }).then(function(candidate){
-          	assert.equal(candidate[0],2, "contains the correct id");
+	    }).then(function(candidate) {
+          	assert.equal(candidate[0], 2, "contains the correct id");
             assert.equal(candidate[1], "Candidate 2", "contains the correct name");
             assert.equal(candidate[2], 0, "contains the correct vote count");
         });
