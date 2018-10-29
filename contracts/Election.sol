7f84786ee715a298b6e13ee51468510c48bffda2
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-10-29 18:27:44 +0800

    vote cast

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,28 +1,37 @@
 pragma solidity ^0.4.24;
 
 contract Election {
-	// Model candidate
-	struct Candidate {
-		uint id;
-		string name;
-		uint voteCount;
-	}
-	//Store
+    // Model candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    //Store
 
-	//Fetch
+    //Fetch
 
-	mapping(uint => Candidate) public candidates;
+    mapping(uint => Candidate) public candidates;
 
-	// Candidate Count Store
-	uint public candidatesCount;
-	
-	function Election() public {
-		addCandidate("Candidate 1");
-		addCandidate("Candidate 2");
-	}
+    // Candidate Count Store
+    uint public candidatesCount;
 
-	function addCandidate(string _name) private {
-		candidatesCount++;
-		candidates[candidatesCount]= Candidate(candidatesCount, _name, 0);
-	}
+    function Election() public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public {
+
+        //record that voter has voted
+
+
+        // update candidate vote count
+        candidates[_candidateId].voteCount++;
+    }
 }
